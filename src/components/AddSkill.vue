a028aa79f04fcbebfe8137adc3989c58f55fef41
Hugo Brook <jhhbrook@gmail.com>
2020-08-03 10:12:14 +0100

    AddSkill component

---
+++ src/components/AddSkill.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mt-2">
+    <div v-if="editing">
+      <input
+        class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+        placeholder="Skill name"
+      />
+      <div class="p-2">
+        <button
+          v-on:click="editing = false"
+          class="bg-blue-500 text-white text-sm py-2 px-4 rounded-full mr-1"
+        >Add this skill</button>
+        <button
+          v-on:click="editing = false"
+          class="border border-gray-400 focus:outline-none text-gray-700 text-sm py-2 px-4 rounded-full"
+        >Cancel</button>
+      </div>
+    </div>
+    <button
+      v-else
+      v-on:click="editing = true"
+      class="border border-gray-400 focus:outline-none text-gray-700 text-sm py-2 px-4 rounded-full"
+    >Add a skill</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddSkill",
+  data: () => {
+    return {
+      editing: false,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -2,50 +2,36 @@
   <div>
     <h1 class="text-3xl mb-6">Skills</h1>
 
-    <section class="mb-10">
+    <section class="mb-16">
       <h2 class="text-xl font-bold underline text-blue-500 mb-2">Required</h2>
       <div v-for="skill in required" :key="skill.name">
         <Skill :name="skill.name" />
       </div>
-      <div class="mt-4">
-        <input
-          class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-          v-if="editingRequired"
-          placeholder="Skill name"
-        />
-        <button
-          v-else
-          v-on:click="editingRequired = true"
-          class="border border-gray-400 focus:outline-none text-gray-700 text-sm py-2 px-4 rounded-full"
-        >Add a skill</button>
-      </div>
+      <AddSkill />
     </section>
 
-    <section class="mb-10">
+    <section class="mb-16">
       <h2 class="text-xl font-bold underline text-blue-500 mb-2">Desired</h2>
       <div v-for="skill in desired" :key="skill.name">
         <Skill :name="skill.name" />
       </div>
-      <button
-        v-on:click="editingDesired = true"
-        class="border border-gray-400 focus:outline-none text-gray-700 text-sm py-2 px-4 rounded-full mt-4"
-      >Add a skill</button>
+      <AddSkill />
     </section>
   </div>
 </template>
 
 <script>
 import Skill from "./Skill.vue";
+import AddSkill from "./AddSkill.vue";
 
 export default {
   name: "Tree",
   components: {
     Skill,
+    AddSkill,
   },
   data: () => {
     return {
-      editingRequired: false,
-      editingDesired: false,
       required: [
         {
           name: "Accounting",
