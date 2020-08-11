f80389ef65a459e07222b5fd6c7621ca7345a6d1
Hugo Brook <jhhbrook@gmail.com>
2020-08-11 11:00:35 +0100

    change structure again

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="text-3xl mb-6">Skills</h1>
-    <section class="mb-16" v-for="group in groups" :key="group.name">
+    <section class="mb-16" v-for="group in groups" :key="group.id">
       <h2 class="text-xl font-bold underline text-blue-500 mb-2">{{group.name}}</h2>
       <div v-for="skill in group.skills" :key="skill.name">
         <Skill :name="skill.name" />
@@ -23,7 +23,7 @@ export default {
     AddSkill,
   },
   data: () => ({
-    groups: skills_store.state.groups,
+    groups: skills_store.state,
   }),
 };
 </script>
--- src/lib/skills_store.js
+++ src/lib/skills_store.js
@@ -1,40 +1,38 @@
 export default {
   debug: true,
   state: {
-    groups: [
-      {
-        name: "Required",
-        editable: false,
-        skills: [
-          {
-            name: "Accounting",
-            desc: "Great description.",
-          },
-          {
-            name: "Law",
-            desc: "Great description.",
-          },
-        ],
-      },
-      {
-        name: "Must have one of",
-        editable: true,
-        skills: [],
-      },
-      {
-        name: "Desired",
-        editable: false,
-        skills: [
-          {
-            name: "Marketing",
-            desc: "Great description.",
-          },
-        ],
-      },
-    ],
+    required: {
+      name: "Required",
+      id: "required",
+      skills: [
+        {
+          name: "Accounting",
+          desc: "Great description.",
+        },
+        {
+          name: "Law",
+          desc: "Great description.",
+        },
+      ],
+    },
+    "must-have-one-of": {
+      name: "Must have one of",
+      id: "must-have-one-of",
+      skills: [],
+    },
+    desired: {
+      name: "Desired",
+      id: "desired",
+      skills: [
+        {
+          name: "Marketing",
+          desc: "Great description.",
+        },
+      ],
+    },
   },
   addSkill({ type, name, desc }) {
     if (this.debug) console.log("skill added triggered");
-    this.state[type].push({ name, desc });
+    this.state[type].skills.push({ name, desc });
   },
 };
